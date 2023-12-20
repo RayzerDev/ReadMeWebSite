f2a5e45bfd5044ac75ffc8bf4d308d9ae53a17ca
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-12-20 11:10:26 +0100

    Ajout du controller Equipe avec une méthode index

---
+++ app/Http/Controllers/EquipeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class EquipeController extends Controller
+{
+    public function index(){
+        $equipe= [
+            'nomEquipe'=>"L'equipes de l'année !",
+            'logo'=>"/public/images/logo.jpg",
+            'slogan'=>"....",
+            'localisation'=>"06E",
+            'membres'=> [
+                [ 'nom'=>"Karamucki",'prenom'=>"Louis", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["validateur","développer", "concepteur"] ],
+                [ 'nom'=>"Allaert",'prenom'=>"Damien", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Guiffroy",'prenom'=>"Matteo", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Rolland",'prenom'=>"Anthony", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Desprez",'prenom'=>"Tom", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Duez",'prenom'=>"Benoit", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Marcezwski",'prenom'=>"Lena", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Morice",'prenom'=>"Noemie", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Renaud",'prenom'=>"Ilona", 'image'=>"equipe/images/test.jpg", 'fonctions'=>["développer", "concepteur"] ],
+            ],
+            'autres'=>"Autre chose",
+        ];
+        return view('equipes.index', ['equipe' => $equipe]);
+    }
+}
