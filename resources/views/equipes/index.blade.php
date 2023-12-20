bb16a23a72c4fc3c289a8c18d3d9e00d34b4490a
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-12-20 11:11:20 +0100

    Création de la vue index pour équipe

---
+++ resources/views/equipes/index.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <div class="container">
+        <h1>{{ $equipe['nomEquipe'] }}</h1>
+        <img src="{{ Vite::asset($equipe['logo']) }}" alt="Logo">
+
+        <p><strong>Slogan:</strong> {{ $equipe['slogan'] }}</p>
+        <p><strong>Localisation:</strong> Salle {{ $equipe['localisation'] }}</p>
+
+        <h2>Membres de l'équipe:</h2>
+        <ul>
+            @foreach($equipe['membres'] as $membre)
+                <li>
+                    <img src="{{ "storage/" . $membre['image'] }}" alt="{{ $membre['nom'] }} {{ $membre['prenom'] }}" width="150px">
+                    <p><strong>Nom:</strong> {{ $membre['nom'] }} {{ $membre['prenom'] }}</p>
+                    <p><strong>Fonctions:</strong> {{ implode(', ', $membre['fonctions']) }}</p>
+                </li>
+            @endforeach
+        </ul>
+
+        <p><strong>Autres:</strong> {{ $equipe['autres'] }}</p>
+    </div>
+</x-layout>
