b37fd05a54954fb81f85de087996f41ee4ccd9f9
Antho <anthony.rolland@proton.me>
2023-12-20 18:51:31 +0100

    :sparkles: + :construction: Ajout de la méthode edit d'un chapitre

--- app/Http/Controllers/ChapitreController.php
+++ app/Http/Controllers/ChapitreController.php
@@ -11,11 +11,34 @@ class ChapitreController extends Controller
 {
     public function show($id){
         $chapitre = Chapitre::findOrFail($id);
-        return view('chapitres.show', ['chapitre' => $chapitre]);
+        return view('chapitres.show', ['chapitre' => $chapitre, 'title' => $chapitre->titrecourt]);
     }
 
-    public function showChapId($idChap){
-        $chapitre = Chapitre::findOrFail($idChap);
-        return view('chapitres.show', ['chapitre' => $chapitre]);
+    public function edit($id){
+        $chapitre = Chapitre::findOrFail($id);
+        return view('chapitres.edit', ['chapitre' => $chapitre, 'title' => "Editer ".$chapitre->titrecourt]);
+    }
+
+    public function update(Request $request, $id){
+        $chapitre = Chapitre::find($id);
+        $chapitre ->titrecourt =  $request->titrecourt;
+        $chapitre ->texte = $request->texte;
+        $chapitre ->media = $request->media;
+        $chapitre ->save();
+        return view('chapitres.show', ['chapitre' => $chapitre, 'title' => $chapitre->titrecourt]);
+    }
+
+    public function create(){
+        return view('chapitres.create', ['title' => "Créer un chapitre"]);
+    }
+
+    public function store(Request $request){
+        $chapitre = new Chapitre();
+        $chapitre ->titre =  $request->titre;
+        $chapitre ->titrecourt =  $request->titrecourt;
+        $chapitre ->texte = $request->texte;
+        $chapitre ->media = $request->media;
+        $chapitre ->save();
+        return view('chapitres.show', ['chapitre' => $chapitre, 'title' => $chapitre->titrecourt]);
     }
 }
