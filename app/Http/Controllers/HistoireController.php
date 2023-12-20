ead949a85c313499279d4c6d60d8bef1caea2858
matteo_guiffroy <matteo_guiffroy@ens.univ-artois.fr>
2023-12-20 16:57:25 +0100

    Filtrage des histoire par genre

--- app/Http/Controllers/HistoireController.php
+++ app/Http/Controllers/HistoireController.php
@@ -5,13 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\Histoire;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class HistoireController extends Controller
 {
-    public function index(){
-        $histoires = Histoire::all();
-        return view('storys.index', ['histoires' => $histoires]);
+    public function index(Request $request)
+    {
+        $cat = $request->input('cat', null);
+        $value = $request->cookie('cat', null);
+        if (!isset($cat)) {
+            if (!isset($value)) {
+                $histoires = Histoire::all();
+                $cat = 'All';
+                Cookie::expire('cat');
+            } else {
+                $histoires = Histoire::where('genre_id', $value)->get();
+                $cat = $value;
+                Cookie::queue('cat', $cat, 10);            }
+        } else {
+            if ($cat == 'All') {
+                $histoires = Histoire::all();
+                Cookie::expire('cat');
+            } else {
+                $histoires = Histoire::where('genre_id', $cat)->get();
+                Cookie::queue('cat', $cat, 10);
+            }
+        }
+        $genres = Histoire::distinct('genre_id')->pluck('genre_id');
+        return view('storys.index',
+            ['titre' => "Liste des Histoires", 'histoires' => $histoires, 'cat' => $cat, 'genres' => $genres]);
     }
+
 
     public function show($idHistoire){
         $histoire = Histoire::find($idHistoire);
--- resources/views/storys/index.blade.php
+++ resources/views/storys/index.blade.php
@@ -1,21 +1,25 @@
 
 <x-layout titre="Liste des Histoires">
+    <script>
+        function submitForm() {
+            document.getElementById("filterForm").submit();
+        }
+    </script>
+
     <div>
         <h1>Liste des Histoires</h1>
-        {{--<form action="{{route('scenes.index')}}" method="get" id="filterForm">
+        <form action="{{route('storys.index')}}" method="get" id="filterForm">
             <div class="input-group">
                 <div class="mb-3">
                     <select name="cat" class="form-select form-control bg-white" onchange="submitForm()">
-                        <option value="All" @if($cat == 'All') selected @endif>-- Toutes équipes --</option>
-                        @foreach($equipes as $equipe)
-                            <option value="{{$equipe}}" @if($cat == $equipe) selected @endif>{{$equipe}}</option>
+                        <option value="All" @if($cat == 'All') selected @endif>-- Tout genres --</option>
+                        @foreach($genres as $genre)
+                            <option value="{{$genre}}" @if($cat == $genre) selected @endif>{{$genre}}</option>
                         @endforeach
                     </select>
                 </div>
-                <a href="{{ route('scenes.index', ['recent' => 1]) }}" class="custom-button text-decoration-none d-flex align-items-center">Les 5 + récents</a>
-                <a href="{{ route('scenes.index', ['top_rated' => 1]) }}" class="custom-button text-decoration-none d-flex align-items-center">Les 5 + notées</a>
             </div>
-        </form>--}}
+        </form>
 
         <div>
             @foreach ($histoires as $histoire)
