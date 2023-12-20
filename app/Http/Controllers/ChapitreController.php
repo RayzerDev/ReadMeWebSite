5f2abbaeeda8e4aeca402cfa380f1080303109be
Antho <anthony.rolland@proton.me>
2023-12-20 16:08:43 +0100

    :sparkles: Ajout de la méthode show pour les chapitres

--- app/Http/Controllers/ChapitreController.php
+++ app/Http/Controllers/ChapitreController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chapitre;
 use App\Models\Histoire;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ChapitreController extends Controller
 {
-    public function show($idHistoire){
-        $histoire = Histoire::find($idHistoire);
+    public function show($id){
+        $chapitre = Chapitre::findOrFail($id);
+        return view('chapitres.show', ['chapitre' => $chapitre]);
+    }
+
+    public function showChapId($idChap){
+        $chapitre = Chapitre::findOrFail($idChap);
+        return view('chapitres.show', ['chapitre' => $chapitre]);
     }
 }
