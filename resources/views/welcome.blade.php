e322ee2bec1e8970b5ae7526453d45574aefc0c9
Antho <anthony.rolland@proton.me>
2023-12-21 04:33:03 +0100

    :sparkles: Modification de la page d'accueil pour que Tom puisse mettre son carroussel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,5 @@
-@extends('templates.app')
-
-@section('content')
-    <div class="container">
-        <h1>ReadMe</h1>
-
-        @if($histoire)
-            <div>
-                <a href="{{ route('storys.show',$histoire->id) }}">
-                <h2>{{ $histoire->titre }}</h2>
-                <p>{{ $histoire->pitch }}</p>
-                <img src="{{ $histoire->photo }}" alt="histoire">
-                </a>
-            </div>
-        @else
-            <p>Aucune histoire disponible pour le moment.</p>
-        @endif
-    </div>
-@endsection
+<x-layout>
+    @section('title')
+        Accueil
+    @endsection
+</x-layout>
