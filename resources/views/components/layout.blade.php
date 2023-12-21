e2e18f6ccc08969548170b60112b7191044febd9
Noémie <noemie9205@orange.fr>
2023-12-21 11:41:00 +0100

    modification css home page  resolution de bug

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -76,7 +76,37 @@
 
 </main>
 
-
+<footer>
+    <div>
+        <h2>Pages</h2>
+        <a href="">Accueil</a>
+        <a href="">Thèmes</a>
+        <a href="">Histoires</a>
+        <a href="">Connexion</a>
+    </div>
+    <div>
+        <h2>Contacts</h2>
+        <a href="">Instagram</a>
+        <a href="">Facebook</a>
+        <a href="">Twitter</a>
+        <a href="">Linkedin</a>
+    </div>
+    <div>
+        <h2>Confidentialité</h2>
+        <a href="">Conditions générales</a>
+        <a href="">Vie privée</a>
+        <a href="">Mentions légales</a>
+        <a href="">Données personnelles</a>
+    </div>
+    <div>
+        <h2>Newsletter</h2>
+        <a href="{{route('histoires.index')}}"><img src = "{{url('storage\images\readme_blanc.png')}}"></a>
+        <div>
+            <input type="mail"/>
+            <button>></button>
+        </div>
+    </div>
+</footer>
 
 </body>
 </html>
